ce55548c3769267fb53b5d35dbdb302ac64f7b65
Ian K <iankoech9@gmail.com>
2020-09-24 08:24:26 +0300

    Added an image with a jumbotron class

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ian:iankoech@localhost/pitch'
+export MAIL_USERNAME='iankoech9@gmail.com'
+export MAIL_PASSWORD='virus2000'
+
+python3.8 manage.py server
